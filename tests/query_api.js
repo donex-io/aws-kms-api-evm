12304d2d148b3e2fd5b58850155da6b32878d008
Lukas Bahrenberg <31018566+LukasBahrenberg@users.noreply.github.com>
2022-04-12 18:14:05 +0200

    Update query_api.js

--- tests/query_api.js
+++ tests/query_api.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 const Web3 = require('web3');
 
-async function getSignature(message) {
-  let req = await axios.post('https://XXX.amazonaws.com/prod/items/', 
+async function getSignature(message, url) {
+  let req = await axios.post(url, 
     {
       "operation": "sign_message", 
       "message": message
@@ -15,26 +15,37 @@ async function getSignature(message) {
 }
 
 async function test(){
+  let args = process.argv
+
+  let message = args[2]
+  console.log("\n" + "Message to be signed: '" + message + "' \n")
+
+  let url = args[3]
+  console.log("API url: " + url + "\n")
+
   var web3 = new Web3(new Web3.providers.HttpProvider('https://localhost:8545'))
   
-  let message = 'hello'
   let message_hash = await web3.eth.accounts.hashMessage(message)
-  console.log(message_hash)
+
+  let res = await getSignature(message, url)
   
-  let res = await getSignature(message)
+  console.log("API response:")
   console.log(res)
+  console.log(" ")
 
   let r = res.r
-  console.log(r + " : " + typeof(r))
   let s = res.s
-  console.log(s + " : " + typeof(s))
+
+  // Determine two address possibilities:
+
+  console.log("Possible addresses:")
 
   let recovered_addr_1 = web3.eth.accounts.recover(message_hash, '0x1c', r, s, true);
-  console.log(recovered_addr_1)
+  console.log("checksum address for v = 27: " + recovered_addr_1)
 
   let recovered_addr_2 = web3.eth.accounts.recover(message_hash, '0x1d', r, s, true);
-  console.log(recovered_addr_2)
-
+  console.log("checksum address for v = 28: " + recovered_addr_2)
+  console.log(" ")
 }
 
 test()
